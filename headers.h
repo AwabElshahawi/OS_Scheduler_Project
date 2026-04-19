15e00269535d4e92b7b3cc31b23a7f486d18bd83
awabelshahawi <awabelshahawi@gmail.com>
2026-04-20 00:06:27 +0200

    Edit 1

--- headers.h
+++ headers.h
@@ -36,94 +36,15 @@ typedef struct PCB
     int endaddress;
 } PCB;
 
-typedef struct Node
-{
-    PCB *pcb;
-    struct Node *next;
-} Node;
-
-typedef struct
-{
-    Node *rear;
-} CircularQueue;
-
-void initQueue(CircularQueue *q)
-{
-    q->rear = NULL;
-}
-
-bool isEmpty(CircularQueue *q)
-{
-    return q->rear == NULL;
-}
-
-void enqueue(CircularQueue *q, PCB *pcb)
-{
-    Node *newNode = (Node *)malloc(sizeof(Node));
-    if (!newNode)
-    {
-        printf("Memory allocation failed\n");
-        return;
-    }
-    if (pcb != NULL)
-    {
-        newNode->pcb = pcb;
-
-        if (isEmpty(q))
-        {
-            newNode->next = newNode;
-            q->rear = newNode;
-        }
-        else
-        {
-            newNode->next = q->rear->next;
-            q->rear->next = newNode;
-            q->rear = newNode;
-        }
-    }
-    else
-    {
-        printf("PCB is NULL \n");
-        free(newNode);
-        return;
-    }
-}
-
-bool dequeue(CircularQueue *q, PCB **retpcb)
-{
-    if (isEmpty(q))
-    {
-        *retpcb = NULL;
-        return false; 
-    }
-
-    Node *temp = q->rear->next;
-    *retpcb = temp->pcb;
-
-    if (q->rear == temp)
-    {
-        q->rear = NULL;
-    }
-    else
-    {
-        q->rear->next = temp->next;
-    }
-
-    free(temp);
-    return true;
-}
 ///==============================
 //don't mess with this variable//
 int * shmaddr;                 //
 //===============================
 
-
-
 int getClk()
 {
     return *shmaddr;
 }
-
 
 /*
  * All process call this function at the beginning to establish communication between them and the clock module.
