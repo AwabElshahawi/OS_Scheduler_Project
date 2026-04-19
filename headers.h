1d1bc3d3511b12a6417c6a88ce04a3b306c67f4f
3atar <mahmoudal2atar@gmail.com>
2026-04-19 22:42:34 +0200

    Lesa Ma5alstesh

--- headers.h
+++ headers.h
@@ -17,7 +17,101 @@ typedef short bool;
 
 #define SHKEY 300
 
+typedef struct PCB
+{
+    int id; 
+    int arrival_time;
+    int runtime;
+    int priority;
+    int state;
+    int remaining_time;
+    int waiting_time;
+    int pid; 
+    int start_time;
+    int finished_time;
+    int stopped_time;
+    int restarted_time;
+    int remainingTimeAfterStop;
+    int startaddress;
+    int endaddress;
+} PCB;
 
+typedef struct Node
+{
+    PCB *pcb;
+    struct Node *next;
+} Node;
+
+typedef struct
+{
+    Node *rear;
+} CircularQueue;
+
+void initQueue(CircularQueue *q)
+{
+    q->rear = NULL;
+}
+
+bool isEmpty(CircularQueue *q)
+{
+    return q->rear == NULL;
+}
+
+void enqueue(CircularQueue *q, PCB *pcb)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode)
+    {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    if (pcb != NULL)
+    {
+        newNode->pcb = pcb;
+
+        if (isEmpty(q))
+        {
+            newNode->next = newNode;
+            q->rear = newNode;
+        }
+        else
+        {
+            newNode->next = q->rear->next;
+            q->rear->next = newNode;
+            q->rear = newNode;
+        }
+    }
+    else
+    {
+        printf("PCB is NULL \n");
+        free(newNode);
+        return;
+    }
+}
+
+bool dequeue(CircularQueue *q, PCB **retpcb)
+{
+    if (isEmpty(q))
+    {
+        *retpcb = NULL;
+        return false; 
+    }
+
+    Node *temp = q->rear->next;
+    *retpcb = temp->pcb;
+
+    if (q->rear == temp)
+    {
+        q->rear = NULL;
+    }
+    else
+    {
+        q->rear->next = temp->next;
+    }
+
+    free(temp);
+    return true;
+}
 ///==============================
 //don't mess with this variable//
 int * shmaddr;                 //
--- process_generator.c
+++ process_generator.c
@@ -6,8 +6,36 @@ int main(int argc, char * argv[])
 {
     signal(SIGINT, clearResources);
     // TODO Initialization
+    CircularQueue *PCBs = (CircularQueue *)malloc(sizeof(CircularQueue));
+    initQueue(PCBs);
+    FILE *f;
+    f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
     // 1. Read the input files.
+    int id, arrival, runtime, priority;
+    fscanf(f, "%*[^\n]\n");
+    while (fscanf(f, "%d %d %d %d", &id, &arrival, &runtime, &priority) == 4)
+    {
+        PCB *p = (PCB *)malloc(sizeof(PCB));
+        p->id = id;
+        p->arrival_time = arrival;
+        p->runtime = runtime;
+        p->priority = priority;
+        p->remaining_time = runtime;
+        enqueue(PCBs, p);
+    }
+    fclose(f);
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
+    printf("\nEnter a scheduling algorithm:\n");
+    printf("1 - Preemptive Highest Priority First (HPF)\n");
+    printf("2 - Highest Priority First (HPF)\n");
+    printf("3 - Round Robin (RR)\n");
+    printf("Please enter the number corresponding to your choice: ");
+
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock
     initClk();
